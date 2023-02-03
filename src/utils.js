c65f1161ece26528983fedf24c57e8ee636c5bc9
Strahinja <strahinjav98@gmail.com>
2023-02-03 18:13:40 +0100

    lesson2 completed and working

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,10 @@ export function uuid () {
 
 export function saveStatePlugin (store) {
   store.subscribe(
-    (mutation, state) => localStorage.setItem('boards', JSON.stringify(state.boards))
+    (mutation, state) => {
+      localStorage.setItem(
+        'boards',
+         JSON.stringify(state.boards))
+    } 
   )
 }
